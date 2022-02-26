236de211a776e63e33666b187ce0aa561674eeb7
Sebastian Rolon <js@rolon.co>
2022-02-25 19:41:50 -0500

    added arg checks for spdk engine

--- bench_fio/benchlib/defaultsettings.py
+++ bench_fio/benchlib/defaultsettings.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+import logging
 import os
 import sys
 import shutil
+
+logger = logging.getLogger(__name__)
 
 
 def get_default_settings():
@@ -56,54 +59,65 @@ def check_settings(settings):
     if shutil.which(settings["fio_path"]) is None:
         if os.path.exists(settings["fio_path"]):
             if not os.access(settings["fio_path"], os.X_OK):
-                print(f"Fio binary at {settings['fio_path']} is not executable. Check its file permissions.")
-                print()
+                logger.error(f"Fio binary at {settings['fio_path']} is not executable. Check its file permissions.")
                 sys.exit(1)
         else:
-            print(f"Fio executable at {settings['fio_path']} not found. Is Fio installed?")
-            print()
+            logger.error(f"Fio executable at {settings['fio_path']} not found. Is Fio installed?")
             sys.exit(1)
 
     """Some basic error handling."""
     if not os.path.exists(settings["template"]):
-        print()
-        print(f"The specified template {settings['template']} does not exist.")
-        print()
+        logger.error(f"The specified template {settings['template']} does not exist.")
         sys.exit(6)
 
     if settings["type"] not in ["device", "rbd"] and not settings["size"]:
-        print()
-        print("When the target is a file or directory, --size must be specified.")
-        print()
+        logger.error("When the target is a file or directory, --size must be specified.")
         sys.exit(4)
 
     if settings["type"] == "directory":
         for item in settings["target"]:
             if not os.path.exists(item):
-                print(f"\nThe target directory ({item}) doesn't seem to exist.\n")
+                logger.error(f"\nThe target directory ({item}) doesn't seem to exist.\n")
                 sys.exit(5)
 
     if settings["type"] == "rbd":
         if not settings["ceph_pool"]:
-            print(
+            logger.error(
                 "\nCeph pool (--ceph-pool) must be specified when target type is rbd.\n"
             )
             sys.exit(6)
 
     if settings["type"] == "rbd" and settings["ceph_pool"]:
         if settings["template"] == "./fio-job-template.fio":
-            print(
+            logger.error(
                 "Please specify the appropriate Fio template (--template).\n\
                     The example fio-job-template-ceph.fio can be used."
             )
             sys.exit(7)
+
+    if "spdk" in settings["engine"]:
+        if len(settings["engine"]) != 1:
+            logger.error("SPDK requires exclusive access to the NVMe device, cannot be used with other engines")
+            sys.exit(9)
+
+        if settings["type"] != "file":
+            logger.error("SPDK fio plugin requires --type=file")
+            sys.exit(9)
+
+        if not ("trtype=" in target for target in settings["target"]):
+            logger.error("SPDK fio plugin requires target filename to be an SPDK Transport Identifier string")
+            sys.exit(9)
+
+        if not settings["env_vars"] or not ("LD_PRELOAD" in env_var for env_var in settings["env_vars"]):
+            logger.error("SPDK fio plugin requires LD_PRELOAD env variable to point to compiled SPDK. Use --env-vars")
+            sys.exit(9)
 
     for mode in settings["mode"]:
         if mode in settings["mixed"]:
             if settings["rwmixread"]:
                 settings["loop_items"].append("rwmixread")
             else:
-                print(
+                logger.error(
                     "\nIf a mixed (read/write) mode is specified, please specify --rwmixread\n"
                 )
                 sys.exit(8)
