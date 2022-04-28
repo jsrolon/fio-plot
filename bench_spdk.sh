3e847f9e710bc125c94d65cf82a9c0d060c57515
Sebastian Rolon <js@rolon.co>
2022-04-27 21:44:28 -0400

    add benchmarking script

---
+++ bench_spdk.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# I'm running the module that is defined in this particular folder,
+# this is why pythonpath is defined as the current working directory
+PYTHONPATH=$(pwd)
+
+if [[ -z "${1}" ]]; then
+    echo "Must provide a name for this run" 1>&2
+    exit 1
+fi
+
+runtime="${1}"
+python3 -m bench_fio \
+        --target "trtype=PCIe traddr=0000.bc.00.0 ns=1" \
+        --size 4g \
+        --type file \
+        --output="/nvme-fio/${runtime}-$(date +%Y_%m_%d_%H_%M_%S)" \
+        --mode read write randread randwrite \
+        --iodepth 1 2 4 8 16 32 64 \
+        --numjobs 1 2 4 8 16 24 31 \
+        --engine spdk \
+        --env-vars "LD_PRELOAD=/nutanix-nvme-bench/src/spdk/build/fio/spdk_nvme" \
+        --time-based
