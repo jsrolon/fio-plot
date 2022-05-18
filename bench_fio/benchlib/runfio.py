42a0a0509895068f1079be2820aaa0ce948e1e2b
Sebastian Rolon <js@rolon.co>
2022-05-18 17:46:06 -0400

    yes

--- bench_fio/benchlib/runfio.py
+++ bench_fio/benchlib/runfio.py
@@ -50,13 +50,15 @@ def run_raw_command(command, env=None):
     os.sched_setaffinity(result.pid, [x for x in range(0, 35) if x % 2 == 0])
     result.wait()
 
+    stdout, stderr = result.communicate()
+
     if result and result.stdout:
         logger.info(result.stdout)
 
     if result.returncode != 0:
         logger.error(result.stdout)
-        logger.error(result.stderr.readlines(100))
-        if result.returncode != 127 and "is_backend" not in result.stderr.readlines(100): # ridiculous workaround for spdk fio plugin
+        logger.error(result.stderr.read())
+        if result.returncode != 127 and "is_backend" not in result.stderr.read(): # ridiculous workaround for spdk fio plugin
             sys.exit(1)
 
     return result
