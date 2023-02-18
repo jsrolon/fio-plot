53712afa4cebd61610915d4ad4dc8cc95b269a1f
Sebastian Rolon <js@rolon.co>
2023-02-18 09:41:07 -0500

    writes are there too

--- fio_plot/__init__.py
+++ fio_plot/__init__.py
@@ -11,30 +11,43 @@ from .fiolib import (
     flightchecks as checks,
     getdata
 )
+import numpy as np
 
 def main():
     # y = [26.66, 17.82, 11.41]
-    y = [26.66, 8.84, 6.41, 11.41]
-    x = range(0, len(y))
-    yerr = [2.86, 3.38, 2.48, 1.69]
+    read_mean = [26.66, 8.84, 6.41, 11.41]
+    read_err = [2.86, 3.38, 2.48, 1.69]
+    x = np.arange(4)
+
+    write_mean = [48.22, 26.95, 11.48, 9.79]
+    write_err = [4.43, 5.03, 2.76, 1.38]
+
+    width = 0.3
 
     # plot:
     fig, ax = plt.subplots()
 
-    plt.rcParams.update({
-        "text.usetex": True,
-        "font.family": "lmodern",
-        # "font.sans-serif": "Helvetica"
-    })
+    # plt.rcParams.update({
+    #     "text.usetex": True,
+    #     "font.family": "lmodern",
+    #     # "font.sans-serif": "Helvetica"
+    # })
 
-    plt.xticks(x, ["fio end-to-end", "fio to nvme driver", "nvme driver to bdev", "bdev to hardware"])
-    plt.xticks(rotation=45, ha='right')
+    plt.yticks(fontsize=20)
+    plt.xticks(x, ["fio end-to-end", "fio to\nnvme driver", "nvme driver\nto bdev", "bdev to\nhardware"])
+    plt.xticks(rotation=45, ha='right', rotation_mode='anchor', fontsize=18)
     # plt.ylim(bottom=0, top=35)
-    plt.ylabel("Time spent at layer (μs)")
-    ax.bar(x=x, height=y, yerr=yerr, capsize=5, color=['grey', '#b6d7a8ff', '#f9cb9cff', '#9fc5e8ff'])
+    plt.ylabel("Time spent at layer (μs)", fontsize=20)
+    ax.bar(x=x-0.2, height=read_mean, yerr=read_err, capsize=5, width=width, color=['grey', '#b6d7a8ff', '#f9cb9cff', '#9fc5e8ff'])
+    ax.bar(x=x+0.2, height=write_mean, yerr=write_err, width=width, capsize=5, hatch="//", linewidth=2, facecolor="none", edgecolor=['grey', '#b6d7a8ff', '#f9cb9cff', '#9fc5e8ff'])
+    plt.rcParams['hatch.linewidth'] = 2
     plt.tight_layout()
-    plt.savefig("read.pdf", format='pdf')
-    plt.show()
+    plt.legend(["Reads", "Writes"], fontsize=16)
+
+    ax.set_frame_on(False)
+
+    plt.savefig("readwrite.pdf", format='pdf')
+    # plt.show()
 
     # settings = {}
     # option_found = False
